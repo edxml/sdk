c5b9415b1a58d12683595f313102b599fec2f291
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2013-03-23 22:13:05 +0100

    Update copyright year

--- edxml-filter.py
+++ edxml-filter.py
@@ -6,7 +6,7 @@
 # 
 #                          EDXML Filtering Utility
 #
-#                  Copyright (c) 2010 - 2012 by D.H.J. Takken
+#                  Copyright (c) 2010 - 2013 by D.H.J. Takken
 #                          (d.h.j.takken@xs4all.nl)
 #
 #          This file is part of the EDXML Software Development Kit (SDK).
--- edxml/EDXMLWriter.py
+++ edxml/EDXMLWriter.py
@@ -5,7 +5,7 @@
 # 
 #                 Python class for EDXML data stream generation
 #
-#                  Copyright (c) 2010 - 2012 by D.H.J. Takken
+#                  Copyright (c) 2010 - 2013 by D.H.J. Takken
 #                          (d.h.j.takken@xs4all.nl)
 #
 #          This file is part of the EDXML Software Development Kit (SDK).
