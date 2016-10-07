c69ce75ec3a0e93436bf471319862922a802250b
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2016-10-07 11:07:55 +0200

    Bump version numbers

--- edxml/version.py
+++ edxml/version.py
@@ -3,6 +3,6 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 #
-# Version 2.1.4:
-__version_info__ = ('2', '1', '4')
+# Version 2.2.0:
+__version_info__ = ('2', '2', '0')
 __version__ = '.'.join(__version_info__)
