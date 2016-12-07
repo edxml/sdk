b066af618de00fd9166a5d42bbd41383789251c0
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2016-12-07 22:58:26 +0100

    Greatly simplified output validation in EDXMLWriter

--- edxml/EDXMLWriter.py
+++ edxml/EDXMLWriter.py
@@ -35,12 +35,10 @@ This module contains the EDXMLWriter class, which is used
 to generate EDXML streams.
 
 """
-from StringIO import StringIO
 
 from lxml import etree
-from lxml.etree import SerialisationError
+from typing import Dict
 
-import EDXMLParser
 import edxml
 from EDXMLBase import *
 
@@ -65,33 +63,14 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
 
     super(EDXMLWriter, self).__init__()
 
+    self._ontology = None
+    self.__schema = None                 # type: etree.RelaxNG
+    self.__eventTypeSchemaCache = {}     # type: Dict[str, etree.RelaxNG]
+    self.__eventTypeSchema = None        # type: etree.RelaxNG
     self.InvalidEvents = 0
     self.Validate = Validate
     self.Output = Output
     self.ElementStack = []
-    self.EDXMLParser = None  # type: EDXMLParser.EDXMLPushParser
-    self.Bridge = None       # type: EDXMLWriter.XMLParserBridge
-
-    if self.Validate:
-
-      # Construct validating EDXML parser
-      self.EDXMLParser = EDXMLParser.EDXMLPushParser(Validate)
-
-      # Construct a bridge that will be used to connect the
-      # output of the XML Generator to the parser / validator.
-      # We use the Bridge to output the XML to, so we have built
-      # a chain that automatically validates the EDXML stream while
-      # it is being generated. The full chain looks like this:
-      #
-      # lxml.etree generator => EDXMLParser => Output
-
-      self.Bridge = EDXMLWriter.XMLParserBridge(self.EDXMLParser, Output)  # type: EDXMLWriter.XMLParserBridge
-
-    else:
-
-      # Validation is disabled. 
-      self.Bridge = Output       # type: EDXMLWriter.XMLParserBridge
-      self.EDXMLParser = None
 
     # Initialize lxml.etree based XML generators. This
     # will write the XML declaration and open the
@@ -111,69 +90,16 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
     self.EventTypes = {}
     self.ObjectTypes = {}
 
-  # The XML parser bridge connects the XML generator to
-  # the XML parser for validating the output. Also, it buffers
-  # output until the output is known to be valid. This helper
-  # class acts like a file object, passing through to a validator.
-
-  class XMLParserBridge(object):
-
-    def __init__(self, Parser, Passthrough = None):
-      self.BufferOutput = False
-      self.Parse = True
-      self.ParseError = None
-      self.Buffer = ''
-      self.Parser = Parser
-      self.Passthrough = Passthrough
-
-    def ReplaceParser(self, Parser):
-      self.Parser = Parser
-
-    def DisableParser(self):
-      self.Parse = False
-
-    def EnableParser(self):
-      self.Parse = True
-
-    def EnableBuffering(self):
-      self.BufferOutput = True
-
-    def ClearBuffer(self):
-      self.Buffer = ''
-
-    def DisableBuffering(self):
-      self.BufferOutput = False
-      if self.Passthrough:
-        self.Passthrough.write(self.Buffer)
-        self.Passthrough.flush()
-        self.Buffer = ''
-
-    def write(self, String):
-      if self.Parse:
-        try:
-          self.Parser.feed(String)
-        except Exception:
-          # We catch parsing exceptions here, because lxml does
-          # not handle them correctly: When the file-like object
-          # that it writes into raises an exception, it will initially
-          # ignore it. On the next write attempt, a generic IO error
-          # is raised, which means that the information about the
-          # original exception is lost. So, we store the original
-          # exception here to allow EDXMLWriter to check for it and
-          # raise a proper, informative exception.
-          self.ParseError = sys.exc_info()
-      if self.Passthrough:
-        if self.BufferOutput:
-          # Add to buffer
-          self.Buffer += String
-        else:
-          # Write directly to output
-          self.Passthrough.write(String)
-          self.Passthrough.flush()
+  def getEventTypeSchema(self, eventTypeName):
+    if eventTypeName not in self.__eventTypeSchemaCache:
+      self.__eventTypeSchemaCache[eventTypeName] = etree.RelaxNG(
+        self._ontology.GetEventType(eventTypeName).generateRelaxNG(self._ontology)
+      )
+    return self.__eventTypeSchemaCache.get(eventTypeName)
 
   def __OuterXMLSerialiserCoRoutine(self):
     """Coroutine which performs the actual XML serialisation"""
-    with etree.xmlfile(self.Bridge, encoding='utf-8') as Writer:
+    with etree.xmlfile(self.Output, encoding='utf-8') as Writer:
       if not 'flush' in dir(Writer):
         raise Exception('The installed version of lxml is too old. Please install version >= 3.4.')
       Writer.write_declaration()
@@ -199,7 +125,7 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
 
   def __InnerXMLSerialiserCoRoutine(self, EventTypeName, EventSourceId):
     """Coroutine which performs the actual XML serialisation of eventgroups"""
-    with etree.xmlfile(self.Bridge, encoding='utf-8') as Writer:
+    with etree.xmlfile(self.Output, encoding='utf-8') as Writer:
       with Writer.element('eventgroup', **{'event-type': EventTypeName, 'source-id': EventSourceId}):
         Writer.flush()
         try:
@@ -227,7 +153,15 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
         'A <definitions> tag must be child of the <events> tag. Did you forget to call CloseEventGroups()?'
       )
 
+    if self._ontology is None:
+      self._ontology = edxmlOntology.Validate()
+    else:
+      # We update our existing ontology, to make sure
+      # that both are compatible.
+      self._ontology.Update(edxmlOntology)
+
     self.XMLWriter.send(edxmlOntology.GenerateXml())
+    self.__eventTypeSchemaCache = {}
 
     return self
 
@@ -259,19 +193,11 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
     self.CurrentElementTypeName = EventTypeName
     self.CurrentElementSourceId = SourceId
 
-    Attributes = {
-      'event-type': EventTypeName,
-      'source-id' : str(SourceId)
-      }
+    if self.Validate:
+      self.__eventTypeSchema = self.getEventTypeSchema(self.CurrentElementTypeName)
 
     self.EventGroupXMLWriter = self.__InnerXMLSerialiserCoRoutine(self.CurrentElementTypeName, str(self.CurrentElementSourceId))
     self.EventGroupXMLWriter.next()
-
-    # Opening an event group triggers validation,
-    # which may raise exceptions.
-    if self.Validate:
-      if self.Bridge.ParseError is not None:
-        raise self.Bridge.ParseError[0], self.Bridge.ParseError[1], self.Bridge.ParseError[2]
 
   def Close(self):
     if len(self.ElementStack) > 0 and self.ElementStack[-1] == 'eventgroup':
@@ -300,102 +226,40 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
     if self.ElementStack[-1] != 'eventgroup':
       self.Error('A <event> tag must be child of an <eventgroup> tag. Did you forget to call OpenEventGroup()?')
 
-    if isinstance(event, edxml.EventElement):
-      event = event.element
-    if not isinstance(event, etree._Element):
+    if isinstance(event, etree._Element):
+      eventElement = event
+    elif isinstance(event, edxml.EDXMLEvent):
       event = edxml.EventElement(
         event.GetProperties(), Parents=event.GetExplicitParents(), Content=event.GetContent()
-      ).element
+      )
+      eventElement = event.element
+    elif isinstance(event, edxml.EventElement):
+      eventElement = event.element
+    else:
+      raise TypeError('Unknown type of event: %s' % str(type(event)))
 
     if self.Validate:
-      # We enable buffering at this point, until we know
-      # for certain that the event is valid. If the event
-      # turns out to be invalid, the application can continue
-      # generating the EDXML stream, skipping the invalid event.
-      self.Bridge.ParseError = None
-      self.Bridge.EnableBuffering()
+      if not self.__eventTypeSchema.validate(eventElement):
+        # Event does not validate. Try to generate a validation
+        # exception that is more readable than the RelaxNG
+        # error is, by validating using the EventType class.
+        try:
+          self._ontology.GetEventType(self.CurrentElementTypeName).validateEventStructure(event)
 
-    # Send element to co-routine, which will use lxml.etree
-    # to write the event.
-    try:
-      self.EventGroupXMLWriter.send(event)
-    except SerialisationError:
-      if self.Bridge.ParseError:
-        # We will handle this below.
-        pass
-      else:
-        # TODO: Occasionally, it still happens that for instance a
-        # keyboard interrupt yields a SerialisationError here, without
-        # saving the original exception in self.Bridge.ParseError.
-        # Figure out why these exceptions are not caught.
-        raise
+          # EventType validator did not find the issue. We have
+          # no other option than to raise the RelaxNG error.
+          raise EDXMLValidationError(self.__eventTypeSchema.error_log.last_error.message)
 
-    if self.Validate and self.Bridge.ParseError is not None:
-      self.RecoverInvalidEvent()
-      self.InvalidEvents += 1
+        except EDXMLValidationError as exception:
+          self.InvalidEvents += 1
+          raise EDXMLValidationError, 'An invalid event was produced:\n%s\n\nThe EDXML validator said: %s\n\n%s' % (
+            etree.tostring(eventElement, pretty_print=True).encode('utf-8'), exception,
+            'Note that this exception is not fatal. You can recover by catching the EDXMLValidationError '
+            'and begin writing a new event.'), sys.exc_info()[2]
 
-      InvalidEventException = self.Bridge.ParseError
-      self.Bridge.ParseError = None
-      if InvalidEventException[0] == EDXMLValidationError:
-        Message = (
-          'An invalid event was produced. The EDXML validator said: %s.\nNote that this exception is not fatal. '
-          'You can recover by catching the EDXMLValidationError and begin writing a new event.'
-        ) % InvalidEventException[1]
-      else:
-        Message = InvalidEventException[1]
-
-      raise InvalidEventException[0], Message, InvalidEventException[2]
-
-    if self.Validate:
-      # At this point, the event has been validated. We disable
-      # buffering, which pushes it to the output.
-      self.Bridge.DisableBuffering()
+    self.EventGroupXMLWriter.send(eventElement)
 
     return self
-
-  def RecoverInvalidEvent(self):
-    # Ok, the EDXMLParser instance that handles the content
-    # from the SAX parser has borked on an invalid event.
-    # This means that the SAX parser cannot continue. We
-    # have no other choice than rebuild the data processing
-    # chain from scratch and restore the state of both the
-    # EDXMLParser instance and XML Generator to the point
-    # where it can output events again.
-
-    DefinitionsBackup = StringIO()
-    EDXMLWriter(DefinitionsBackup, False).AddOntology(self.EDXMLParser.getOntology())
-
-    # Create new SAX parser and validating EDXML parser
-    self.EDXMLParser = EDXMLParser.EDXMLPushParser()
-
-    # Replace the parser in the bridge
-    self.Bridge.ReplaceParser(self.EDXMLParser)
-
-    # We need to push some XML into the XMLGenerator and into
-    # the bridge, but we do not want that to end up in the
-    # output. So, we enable buffering and clear the buffer later.
-    self.Bridge.EnableBuffering()
-
-    # Push the EDXML definitions header into the bridge, which
-    # will restore the state of the EDXMLParser instance.
-    self.Bridge.write('%s<eventgroups>' % DefinitionsBackup.getvalue())
-
-    # Close the XML generator that is generating the
-    # current event group. We do not want the resulting
-    # closing eventgroup tag to reach the parser.
-    self.Bridge.DisableParser()
-    self.EventGroupXMLWriter.close()
-    self.Bridge.EnableParser()
-
-    # Create a new XML generator.
-    self.EventGroupXMLWriter = self.__InnerXMLSerialiserCoRoutine(self.CurrentElementTypeName, str(self.CurrentElementSourceId))
-    self.EventGroupXMLWriter.next()
-
-    # Discard all buffered output and stop buffering. Now,
-    # our processing chain is in the state that it was when
-    # we started producing events.
-    self.Bridge.ClearBuffer()
-    self.Bridge.DisableBuffering()
 
   def CloseEventGroups(self):
     """Closes a previously opened <eventgroups> section"""
@@ -403,9 +267,3 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
     self.ElementStack.pop()
 
     self.XMLWriter.close()
-
-    if self.EDXMLParser:
-      # This triggers well-formedness validation
-      # if validation is enabled.
-      self.EDXMLParser._close()
-
