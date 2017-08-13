c519ed2c08728907d56f9b57ab411321d49b7cf3
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2017-08-13 22:36:22 +0200

    Generate documentation for Transcoder and TranscoderMediator classes

--- edxml/transcode/__init__.py
+++ edxml/transcode/__init__.py
@@ -1,4 +1,12 @@
 """
 This sub-package contains several classes to ease development of transcoders that convert
 various types of input data (like JSON records) into EDXML output streams.
+
+..  autoclass:: Transcoder
+    :members:
+    :show-inheritance:
+
+..  autoclass:: TranscoderMediator
+    :members:
+    :show-inheritance:
 """
