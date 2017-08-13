4f7f36e37bd16b0b13ab29cfc4bb14c8e9124ca5
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2017-08-13 22:30:55 +0200

    Add doc describing how to generate dynamic source definitions

---
+++ doc/patterns/dynamic-edxml-source.py
@@ -0,0 +1,32 @@
+import sys, json
+
+from dateutil.parser import parse
+
+from edxml import SimpleEDXMLWriter, EDXMLEvent
+from edxml.ontology import Ontology
+
+myOntology = Ontology()
+
+# ...add event type definitions here...
+
+writer = SimpleEDXMLWriter(sys.stdout)
+writer.SetOntology(myOntology)
+
+definedUri = []
+
+for line in sys.stdin:
+  properties = json.loads(line)
+
+  parsedDateTime = parse(properties['datetime'])
+  sourceUri = '/some/namespace/' + parsedDateTime.strftime('%Y/%m/')
+
+  if sourceUri not in definedUri:
+    # We have not defined this source URI yet.
+    sourceDesc = parsedDateTime.strftime('Data generated in %B %Y')
+    acquisitionDate = parsedDateTime.strftime('%Y%m%d')
+    myOntology.CreateEventSource(sourceUri, Description=sourceDesc, AcquisitionDate=acquisitionDate)
+    definedUri.append(sourceUri)
+
+  writer.AddEvent(EDXMLEvent(properties))
+
+writer.Close()
