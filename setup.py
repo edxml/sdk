aec756b5c962f8f1535563e8c82efc5ae081c34d
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2017-08-24 21:54:24 +0200

    Adjust setup.py to have pip install stub files

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
   # installed, specify them here. If using Python 2.6 or less, then these
   # have to be included in MANIFEST.in as well.
   package_data={
-    'edxml': ['schema/edxml-schema-3.0.0.rng'],
+    'edxml': ['schema/edxml-schema-3.0.0.rng', '**/*.pyi']
   },
 
   # List run-time dependencies here. These will be installed by pip when your
