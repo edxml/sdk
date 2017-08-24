298c38ebd5f5b620abb86bef5257f041892c76a4
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2017-08-24 21:55:37 +0200

    Remove trailing space

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
   },
 
   # List run-time dependencies here. These will be installed by pip when your
-  # project is installed. 
+  # project is installed.
   # See https://pip.pypa.io/en/latest/reference/pip_install.html#requirements-file-format
   # For an analysis of "install_requires" vs pip's requirements files see:
   # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
