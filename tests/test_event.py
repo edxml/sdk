d0bebf1757f76738ba912f147105319ff74ac3e1
Edwin Smulders <edwin.smulders@northwave.nl>
2018-07-09 17:25:35 +0200

    Make tests/ flake8-compatible

--- tests/test_event.py
+++ tests/test_event.py
@@ -2,8 +2,10 @@
 # the line above is required for inline unicode
 from edxml.event import EDXMLEvent
 
+
 def test_event_init():
     content = u"Två dagar kvar🎉🎉"
-    event = EDXMLEvent([], EventTypeName = None, SourceUri = None, Parents = None, Content=content)
+    event = EDXMLEvent([], EventTypeName=None, SourceUri=None,
+                       Parents=None, Content=content)
 
     assert event.Content == content
