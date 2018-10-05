ff640afcd7801239b34a13153a5e27888ac3039e
Edwin Smulders <edwin.smulders@northwave.nl>
2018-10-05 15:52:19 +0200

    add unit tests for the different merge strategies

--- tests/test_event.py
+++ tests/test_event.py
@@ -201,3 +201,118 @@ def test_merge_parents(ontology, objecttype, eventtype_unique, eventproperty_uni
     # This checks fails until the next commit, because during writing
     # we discovered a bug where it returned as being changed
     assert not changed3
+
+
+def test_merge_minmax_datetime(ontology, eventtype_unique, datetimeobjecttype):
+    # create a datetime prop with strategy min
+    prop = eventtype_unique.create_property('prop', datetimeobjecttype.get_name()).set_merge_strategy('min')
+
+    e1 = EDXMLEvent({'prop': ['2018-10-03T15:15:31.000000Z']}, 'testeventtypeunique')
+    e2 = EDXMLEvent({'prop': ['2018-10-03T15:15:32.000000Z']}, 'testeventtypeunique')
+    e3 = EDXMLEvent({'prop': ['2018-10-03T15:15:30.000000Z']}, 'testeventtypeunique')
+
+    # test the merge with min
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert copy['prop'].pop() == '2018-10-03T15:15:30.000000Z'
+
+    # set to max
+    prop.set_merge_strategy('max')
+
+    # test the merge with max
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert copy['prop'].pop() == '2018-10-03T15:15:32.000000Z'
+
+
+@pytest.fixture(params=['number:float', 'number:double', 'number:decimal:3:2'])
+def floatobjecttype(request, ontology):
+    return ontology.create_object_type('floatobjecttype', data_type=request.param)
+
+
+def test_merge_minmax_float(ontology, eventtype_unique, floatobjecttype):
+    # create a float prop with strategy min
+    prop = eventtype_unique.create_property('prop', floatobjecttype.get_name()).set_merge_strategy('min')
+
+    e1 = EDXMLEvent({'prop': ['3.10']}, 'testeventtypeunique')
+    e2 = EDXMLEvent({'prop': ['10.20']}, 'testeventtypeunique')
+    e3 = EDXMLEvent({'prop': ['2.09']}, 'testeventtypeunique')
+
+    # test the merge with min
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert float(copy['prop'].pop()) == 2.09
+
+    # set to max
+    prop.set_merge_strategy('max')
+
+    # test the merge with max
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert float(copy['prop'].pop()) == 10.20
+
+
+@pytest.fixture(params=['number:tinyint', 'number:smallint', 'number:mediumint', 'number:int', 'number:bigint'])
+def intobjecttype(request, ontology):
+    return ontology.create_object_type('intobjecttype', data_type=request.param)
+
+
+def test_merge_minmax_int(ontology, eventtype_unique, intobjecttype):
+    # create a float prop with strategy min
+    prop = eventtype_unique.create_property('prop', intobjecttype.get_name()).set_merge_strategy('min')
+
+    e1 = EDXMLEvent({'prop': ['3']}, 'testeventtypeunique')
+    e2 = EDXMLEvent({'prop': ['10']}, 'testeventtypeunique')
+    e3 = EDXMLEvent({'prop': ['2']}, 'testeventtypeunique')
+
+    # test the merge with min
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert int(copy['prop'].pop()) == 2
+
+    # set to max
+    prop.set_merge_strategy('max')
+
+    # test the merge with max
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3], ontology)
+    assert changed
+    assert int(copy['prop'].pop()) == 10
+
+
+def test_merge_add(ontology, eventtype_unique, eventproperty_unique, objecttype):
+    eventtype_unique.create_property('testeventproperty', objecttype.get_name()) \
+        .set_multi_valued(True) \
+        .set_merge_strategy('add')
+
+    e1 = EDXMLEvent({
+            'testeventpropertyunique': ["test"],
+            'testeventproperty': ["value1"]
+        }, 'testeventtypeunique')
+    # multivalued
+    e2 = EDXMLEvent({
+            'testeventpropertyunique': ["test"],
+            'testeventproperty': ["value2", "value3", "value4"]
+        }, 'testeventtypeunique')
+    # same value as above
+    e3 = EDXMLEvent({
+            'testeventpropertyunique': ["test"],
+            'testeventproperty': ["value3"]
+        }, 'testeventtypeunique')
+    # empty value
+    e4 = EDXMLEvent({
+            'testeventpropertyunique': ["test"],
+            'testeventproperty': []
+        }, 'testeventtypeunique')
+
+    copy = deepcopy(e1)
+    changed = copy.merge_with([e2, e3, e4], ontology)
+
+    assert changed
+    assert copy["testeventpropertyunique"].pop() == "test"
+    assert copy["testeventproperty"] == set(["value1", "value2", "value3", "value4"])
