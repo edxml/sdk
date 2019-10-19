dddf40ad39a832b630f50477cb610653a342b580
D.H.J. Takken <d.h.j.takken@xs4all.nl>
2019-10-19 21:29:04 +0200

    EDXMLWriter is now a context manager

--- edxml/EDXMLWriter.py
+++ edxml/EDXMLWriter.py
@@ -134,6 +134,12 @@ class EDXMLWriter(EDXMLBase, EvilCharacterFilter):
         self.__event_types = {}
         self.__object_types = {}
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
     def get_output(self):
         return self.__output
 
