83c35d4946539d677ae4b41d6f918232b4c3dac5
D.H.J. Takken <dik.takken@edxml.org>
2022-02-21 17:26:12 +0100

    Some type fixes in stub

--- edxml/event.pyi
+++ edxml/event.pyi
@@ -33,7 +33,7 @@ class EDXMLEvent(MutableMapping):
 
     def __init__(self, properties: Optional[Dict[str, Union[Any, Set[Any]]]] = None,
                  event_type_name: str = None, source_uri: str = None,
-                 parents: List[str] = None, attachments: Dict[str, Dict[str, str]] = None,
+                 parents: List[str] = None, attachments: Dict[str, Union[str, Dict[str, str]]] = None,
                  foreign_attribs: Dict[str, str]=None) -> None:
 
         self._properties = ...    # type: PropertySet
@@ -83,7 +83,7 @@ class EDXMLEvent(MutableMapping):
     @classmethod
     def create(cls, properties: Dict[str, Union[Any, Set[Any]]] = None,
                event_type_name: str = None, source_uri: str = None,
-               parents: List[str] = None, attachments: Dict[str, Dict[str, str]] = None) -> 'EDXMLEvent': ...
+               parents: List[str] = None, attachments: Dict[str, Union[str, Dict[str, str]]] = None) -> 'EDXMLEvent': ...
 
     def get_type_name(self) -> str: ...
 
@@ -126,7 +126,7 @@ class ParsedEvent(EDXMLEvent, etree.ElementBase):
 
     def __init__(self, properties: Dict[str, Union[Any, Set[Any]]] = None,
                  event_type_name: str = None, source_uri: str = None,
-                 parents: List[str] = None, attachments: Dict[str, Dict[str, str]] = None,
+                 parents: List[str] = None, attachments: Dict[str, Union[str, Dict[str, str]]] = None,
                  foreign_attribs: Dict[str, str]=None) -> None:
         super().__init__(properties, event_type_name, source_uri, parents, attachments)
         self.__properties = ...  # type: Dict[str, Set[str]]
@@ -170,7 +170,7 @@ class EventElement(EDXMLEvent):
 
     def __init__(self, properties: Dict[str, Union[Any, Set[Any]]] = None,
                  event_type_name: str = None, source_uri: str = None,
-                 parents: List[str] = None, attachments: Dict[str, Dict[str]] = None,
+                 parents: List[str] = None, attachments: Dict[str, Union[str, Dict[str, str]]] = None,
                  foreign_attribs: Dict[str, str]=None) -> None:
         super().__init__(properties, event_type_name, source_uri, parents, attachments)
         self.__properties = ...  # type: Dict[str, Set[str]]
